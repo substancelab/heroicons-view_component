39210505c6a5eb2203b9db5d5de9f118890ed898
Jakob Skjerning <jakob@mentalized.net>
2022-09-16 20:19:19 +0200

    Initialize heroicons-view_component
        $ bundle gem heroicons-view_component

---
+++ lib/heroicons/view_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "view_component/version"
+
+module Heroicons
+  module ViewComponent
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
